d8865e22f0d2316e30f2f5fbf482fee00c9e9312
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-13 21:15:59 +0900

    wip

---
+++ README.md
@@ -0,0 +1,8 @@
+# AWS API Gateway를 커스텀 도메인으로 배포하는 방법을 소개합니다.
+
+## 사용한 플러그인
+
+- serverless-add-api-key
+- serverless-domain-manager
+
+## 디테일한 내용 작성 예정
---
+++ todo/create.ts
@@ -0,0 +1,40 @@
+'use strict'
+
+import { DynamoDB } from 'aws-sdk';
+import { uuid } from 'uuidv4';
+
+const dynamoDb  = new DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+    const timestamp = new Date().getTime()
+    const data = JSON.parse(event.body)
+    if (typeof data.edgeId !== 'string'){
+        console.error("Validation failed");
+        callback(new Error("can't parse body"));
+        return
+    }
+
+    const params =  {
+        TableName : process.env.DYNAMODB_TABLE,
+        Item: {
+            todoId: uuid(),
+            content: data.content,
+            createAt: timestamp,
+            updatedAt: timestamp
+        }
+    }
+
+    dynamoDb.put(params, (error, result) => {
+        if (error)  {
+            console.error(error);
+            callback(new Error('Cound not put config item'));
+            return
+        }
+
+        const response = {
+            statusCode:200,
+            body: JSON.stringify(params.Item)
+        }
+        callback(null, response)
+    })
+}
